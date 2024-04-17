65e4292ee8125b4b3b74b9dbdbbbae566f5031f0
Giordan Zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-04-17 20:22:44 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Giordan Zeina All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Created by: Giordan Zeina
+// Created on: March 2024
+
+'use strict'
+/**
+ * This function calculates volume of a pyramid
+ */
+function myButtonClicked() {
+  // input
+  const length = parseFloat(document.getElementById('length').value)
+  const width = parseFloat(document.getElementById('width').value)
+  const height = parseFloat(document.getElementById('height').value)
+
+
+  // process
+  const volume = (length * width * height) / 3
+
+  // output
+  document.getElementById('volume').innerHTML = 'The Volume is: ' + volume.toFixed(2) + ' mm³'
+}
